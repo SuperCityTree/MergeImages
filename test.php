18dea73edc1871c8b37163944b529116dff6bc94
Carlos Alberto Benitez <cabenitez83@gmail.com>
2016-11-10 23:43:08 -0300

    delete unused line.

--- test.php
+++ test.php
@@ -2,7 +2,6 @@
 
 require_once __DIR__ . '/mergeImages.php';
 
-// $images = ['../img2.jpg', '../img7.jpg', '../img8.png', 'https://www.autoefectivo.com/img/auto-bien.png', '../img9.gif'];
 $remoteImage = 'http://kraken-php.com/build/img/index/logo-php-adbac78231.png';
 $images = ['img/boostrap.jpg','img/jquery.png','img/css.jpg', $remoteImage, 'img/html5.png'];
 $path = 'files/';
